64a73a6677a2e94a5d4875fdd636123f8ad0f0f9
HANJAEWOONG1233 <hanjaewoong1233@gmail.com>
2023-08-17 00:45:51 +0900

    first version

--- c_language_stage_5.c
+++ c_language_stage_5.c
@@ -2,35 +2,39 @@
 
 int main(void) // 입력형태void 출력 형태:int, main이라는 함수
 {
-    int num1=0x47,num2=0x43;// 0x로 시작하면 16진수로 인식
-    int num3=032, num4=024; // 0으로 시작하면 8진수로 인식
+    char ch=9;
+    int inum=1052;
+    double dnum=3.1415;
+    printf("변수 ch의 크기: \n",sizeof(ch));
+    printf("변수 inum의 크기: \n",sizeof(inum));
+    printf("변수 dnum의 크기: \n",sizeof(dnum));
+    printf("변수 char의 크기: \n",sizeof(char));
+    printf("변수 int의 크기: \n",sizeof(int));
+    printf("변수 float의 크기: \n",sizeof(double));
 
-    printf("0x47의 10진수 정수 값: %d\n",num1);
-    printf("0x43의 10진수 정수 값: %d\n",num2);
-    printf("032의 10진수 정수 값: %d\n",num3);
-    printf("024의 10진수 정수 값: %d\n",num4);
+    char ch1='A', ch2=65;
+    int ch3='Z', ch4=95;
+    printf("%c %d \n", ch1, ch1);
+    printf("%c %d \n", ch2, ch2);
+    printf("%c %d \n", ch3, ch3);
+    printf("%c %d \n", ch4, ch4);
 
-    int num1=15; //00001111
-    int num2=20; //00010100
-    int num3 = num1&num2; //num1과 num2의 비트 단위 & 연산 1 1 일떄만 1 반환
-    printf("and 연산의 결과: %d \n",num3);
+    int num = 30 +40; //리터럴 상수: 이름이 없는 상수
+    const int MAX=100; //값의 변경이 안되는 상수 CONST 상수
 
-    int num4 =num1 | num2;
-    printf("or 연산의 결과: %d \n",num4); //num1과 num2의 비트 단위 | 연산 0 1  1 0  1 1 일때 1 반환
+    double num1=245;
+    int num2=3.1415;
+    int num3=129;
+    char ch=num3;
+    print("정수 245를 실수로: %f \n",num1);
+    print("실수 3.1415를 정수로: %d \n",num2);
+    print("큰 정수 129를 실수로: %d \n",ch); //형 변환 하면서 실수형>>정수형으로 바뀌면 뒤에 4바이트 짤린만큼 데이터 손실
 
-    int num5 =num1^num2;
-    printf("xor 연산의 결과 : %d \n",num5); //num1 과 num2의 비트 단위 ^ 연산  0  1   1  0  일떄 1 반환(두개의 비트가 서로 다른경우에 1 반환)
+    short num1=15,num2=25;
+    short num3=num1+num2;//num1과 num2가 int형으로 형 변환
 
-    int num6= ~num1; //msb도 반전이 되며 보수연산이 진행됨 0>>1, 1>>0 바뀜.
-    printf("~num1의 연산의 결과: %d\n",num6);
+    double num1=5.15+19;//int형 정수를 double형 실수로 형 변환
 
-    int result1 =num1<<1; //num1의 비트 열을 왼쪽으로 1칸씩 이동, 결과값 :30
-    int result1 =num1<<2; //num1의 비트 열을 왼쪽으로 2칸씩 이동, 결과값 :60
-    int result1 =num1<<3; //num1의 비트 열을 왼쪽으로 3칸씩 이동, 결과값 :120
-
-    int num1=-16;
-    int result2 =num1>>1; //num1의 비트 열을 오른쪽으로 1칸씩 이동, 결과값 :-8
-    int result2 =num1>>2; //num1의 비트 열을 오른쪽으로 2칸씩 이동, 결과값 :-4
-    int result2 =num1>>3; //num1의 비트 열을 오른쪽으로 3칸씩 이동, 결과값 :-2
-
+    int num4=3;
+    double num5=2.5*(double)num4;//명시적 형 변환.>> 강제적으로 형을 변환시키는것
 }
